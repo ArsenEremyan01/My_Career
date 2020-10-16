093f727d596219f6ff90dca2a0727fd393b4c007
ArsenEremyan01 <64101783+ArsenEremyan01@users.noreply.github.com>
2020-10-16 23:40:06 +0300

    Update Task_6.py

--- Linked_list/Task_6.py
+++ Linked_list/Task_6.py
@@ -12,3 +12,21 @@ class Solution:
         head = prev
         return head
 
+#recursive
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        if head.next is None:
+            return head
+        res = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return res
+        # def reverse_helper(node, prev = None):
+        #     if not node:
+        #         return prev
+        #     after = node.next
+        #     node.next = prev
+        #     return reverse_helper(after,node)
+        # return reverse_helper(head)
